6c5ac7adde9646931e014cc14835f525d9e984c2
leandrozulis <leandro_zulli@hotmail.com>
2022-07-16 13:07:12 -0300

    Projeto Finalizado

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import readLine from 'readline-sync';
-import {opcaoUm, opcaoTres} from './filtragemDeDados/listaDosSalarios.js';
-import opcaoDois from './filtragemDeDados/listaDasInflacoes.js';
-import salarios from './listaDeDados/listaDeSalarios.js';
+import salarioMinimo from './listaDeDados/listaDeSalarios.js';
+import inflacao from './listaDeDados/listaDeInflacoes.js';
 
 console.log(`Escolha uma das alternativas:\n\n`);
 
@@ -12,18 +11,74 @@ console.log(`3 - Comparação entre o percentual de aumento salarial e o IPCA\n\
 let escolha = readLine.question(`Digite o numero da sua escolha: `);
 console.log("\n");
 
+escolha = Number(escolha);
+
+let anoLabel = `Ano: `;
+let salarioLabel = `Salário mínimo: `;
+let crescimentoLabel = `Crescimento Salarial: `;
+let ipcaLabel = `Inflação IPCA: `;
+
+anoLabel = anoLabel.padEnd(30,'.');
+salarioLabel = salarioLabel.padEnd(30,'.');
+crescimentoLabel = crescimentoLabel.padEnd(30,'.');
+ipcaLabel = ipcaLabel.padEnd(30,'.');
+
 switch(escolha){
 
-  case '1':
-    opcaoUm(salarios);
+  case 1:
+    for (let i = 0; i <= salarioMinimo.length-1; i++) {
+      let ano = salarioMinimo[i].ano;
+      let salario = salarioMinimo[i].salario;
+
+      let salarioFormatado = salario.toFixed(2).replace(".", ',');
+
+      console.log("\n");
+      console.log(anoLabel+ano);
+      console.log(salarioLabel+"R$ "+salarioFormatado);
+    }
     break;
 
-  case '2':
-    opcaoDois(salarios);
+  case 2:
+    for (let i = 0; i <= inflacao.length-1; i++) {
+      let ano = inflacao[i].ano;
+      let ipca = inflacao[i].porcentagem;
+
+      let ipcaFormatado = ipca.toFixed(2).replace(".", ',');
+
+      console.log("\n");
+      console.log(anoLabel+ano);
+      console.log(ipcaLabel+ipcaFormatado+"%");
+    }
     break;
 
-  case '3':
-    opcaoTres(salarios);
+  case 3:    
+  for (let i = 0; i <= salarioMinimo.length-1; i++) {
+      let ano = salarioMinimo[i].ano;
+      let salario = salarioMinimo[i].salario;
+      let percentualDeCrescimento;
+      let crescimentoFormatado;
+
+      if (i > 0) {
+        let salarioAnterior = salarioMinimo[i-1].salario;
+        let diferenca = salario - salarioAnterior;
+
+        percentualDeCrescimento = (diferenca/salarioAnterior) * 100;
+        crescimentoFormatado = percentualDeCrescimento.toFixed(2).replace(".",',');
+      } else {
+        crescimentoFormatado = "-";
+      }
+
+      let ipca = inflacao[i].porcentagem;
+
+      let salarioFormatado = salario.toFixed(2).replace(".",",");
+      let ipcaFormatado = ipca.toFixed(2).replace(".", ',');
+
+      console.log("\n");
+      console.log(anoLabel+ano);
+      console.log(salarioLabel+"R$ "+salarioFormatado);
+      console.log(crescimentoLabel+crescimentoFormatado+"%");
+      console.log(ipcaLabel+ipcaFormatado+"%");
+    }
     break;
 
   default:
--- listaDeDados/listaDeInflacoes.js
+++ listaDeDados/listaDeInflacoes.js
@@ -1,15 +1,15 @@
 const inflacaoIPCAAno = [
-	{percentual: 5.91,	ano: 2010 },
-	{percentual: 6.50,	ano: 2011 },
-	{percentual: 5.84,	ano: 2012 },
-	{percentual: 5.91,	ano: 2013 },
-	{percentual: 6.41,	ano: 2014 },
-	{percentual: 10.67,	ano: 2015 },
-	{percentual: 6.29, ano: 2016 },
-	{percentual: 2.95,	ano: 2017 },
-	{percentual: 3.75,	ano: 2018 },
-	{percentual: 4.31,	ano: 2019 },
-	{percentual: 4.52,	ano: 2020}
+	{ porcentagem: 5.91,	ano: 2010 },
+	{ porcentagem: 6.50,	ano: 2011 },
+	{ porcentagem: 5.84,	ano: 2012 },
+	{ porcentagem: 5.91,	ano: 2013 },
+	{ porcentagem: 6.41,	ano: 2014 },
+	{ porcentagem: 10.67,	ano: 2015 },
+	{ porcentagem: 6.29, ano: 2016 },
+	{ porcentagem: 2.95,	ano: 2017 },
+	{ porcentagem: 3.75,	ano: 2018 },
+	{ porcentagem: 4.31,	ano: 2019 },
+	{ porcentagem: 4.52,	ano: 2020}
 ];
 
 export default inflacaoIPCAAno;
